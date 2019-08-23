bd8aa68a397a86c1c5f7decd7b303b46bc3ab8b5
Chuan Liu <chuan.liu@oracle.com>
2019-08-22 18:36:00 -0700

    Add and apply editorconfig

--- bin/install.sh
+++ bin/install.sh
@@ -3,50 +3,50 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 main() {
-    set -o errexit
-    echo "Installing packages..."
-    read -p "Proceed? [y/N] : " yn
-    case $yn in
-        Y|y) install;;
-        *) ;;
-    esac
+  set -o errexit
+  echo "Installing packages..."
+  read -p "Proceed? [y/N] : " yn
+  case $yn in
+    Y|y) install;;
+    *) ;;
+  esac
 
-    DOTFILES_DIR="$(realpath ${BIN_DIR}/..)"
+  DOTFILES_DIR="$(realpath ${BIN_DIR}/..)"
 
-    echo "Configuring zsh..."
-    config_zsh
+  echo "Configuring zsh..."
+  config_zsh
 
-    echo "Configuring tmux..."
-    config_tmux
+  echo "Configuring tmux..."
+  config_tmux
 
-    echo "Configuring vim..."
-    config_vim
+  echo "Configuring vim..."
+  config_vim
 
-    echo "Configuring emacs..."
-    config_emacs
+  echo "Configuring emacs..."
+  config_emacs
 
-    echo "Configuring alacritty..."
-    config_alacritty
+  echo "Configuring alacritty..."
+  config_alacritty
 
-    echo "Done."
+  echo "Done."
 }
 
 install() {
-    UNAME=$(uname | tr "[:lower:]" "[:upper:]")
+  UNAME=$(uname | tr "[:lower:]" "[:upper:]")
 
-    case $UNAME in
-        DARWIN) install_mac ;;
+  case $UNAME in
+    DARWIN) install_mac ;;
 
-        LINUX)
-            DISTRO=$(cat /etc/os-release | grep ^ID= | cut -d '=' -f 2)
-            case $DISTRO in
-                ubuntu) install_linux ;;
-                *) give_up ;;
-            esac
-            ;;
-
+    LINUX)
+      DISTRO=$(cat /etc/os-release | grep ^ID= | cut -d '=' -f 2)
+      case $DISTRO in
+        ubuntu) install_linux ;;
         *) give_up ;;
-    esac
+      esac
+      ;;
+
+    *) give_up ;;
+  esac
 }
 
 give_up() {
@@ -55,21 +55,21 @@ give_up() {
 }
 
 install_mac() {
-    # Install Homebrew.
-    if test ! $(which brew); then
-        echo "Installing Homebrew (https://brew.sh/)"
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    # Update Homebrew.
-    brew update
-    # Install packages.
-    brew ls --versions coreutils || brew install coreutils
-    brew ls --versions bat || brew install bat
-    brew ls --versions fd || brew install fd
-    brew ls --versions ripgrep || brew install ripgrep
-    brew ls --versions zsh || brew install zsh
-    brew ls --versions tmux || brew install tmux
-    brew ls --versions macvim || brew install macvim
+  # Install Homebrew.
+  if test ! $(which brew); then
+    echo "Installing Homebrew (https://brew.sh/)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  # Update Homebrew.
+  brew update
+  # Install packages.
+  brew ls --versions coreutils || brew install coreutils
+  brew ls --versions bat || brew install bat
+  brew ls --versions fd || brew install fd
+  brew ls --versions ripgrep || brew install ripgrep
+  brew ls --versions zsh || brew install zsh
+  brew ls --versions tmux || brew install tmux
+  brew ls --versions macvim || brew install macvim
 }
 
 config_alacritty() {
@@ -83,49 +83,49 @@ config_alacritty() {
 }
 
 install_linux() {
-    sudo apt-get update && sudo apt-get -y upgrade
-    sudo apt-get -y install aptitude
-    sudo aptitude -y install fd-find ripgrep zsh tmux vim
+  sudo apt-get update && sudo apt-get -y upgrade
+  sudo apt-get -y install aptitude
+  sudo aptitude -y install fd-find ripgrep zsh tmux vim
 
-    BAT_LATEST_VERSION=0.11.0
-    BAT_INSTALLED_VERSION=$(dpkg-query -W bat 2>/dev/null | awk '{ print $2 }')
-    if [[ "${BAT_INSTALLED_VERSION}" < "${BAT_LATEST_VERSION}" ]]; then
-        BAT_PKG=bat_${BAT_LATEST_VERSION}_amd64.deb
-        wget https://github.com/sharkdp/bat/releases/download/v${BAT_LATEST_VERSION}/${BAT_PKG}
-        sudo dpkg -i ${BAT_PKG}
-        rm -f ${BAT_PKG}
-    else
-        echo "bat ${BAT_INSTALLED_VERSION} is already installed. Skipping..."
-    fi
+  BAT_LATEST_VERSION=0.11.0
+  BAT_INSTALLED_VERSION=$(dpkg-query -W bat 2>/dev/null | awk '{ print $2 }')
+  if [[ "${BAT_INSTALLED_VERSION}" < "${BAT_LATEST_VERSION}" ]]; then
+    BAT_PKG=bat_${BAT_LATEST_VERSION}_amd64.deb
+    wget https://github.com/sharkdp/bat/releases/download/v${BAT_LATEST_VERSION}/${BAT_PKG}
+    sudo dpkg -i ${BAT_PKG}
+    rm -f ${BAT_PKG}
+  else
+    echo "bat ${BAT_INSTALLED_VERSION} is already installed. Skipping..."
+  fi
 }
 
 config_zsh() {
-    if [[ ! -d $HOME/.antigen ]]; then
-        mkdir -p $HOME/.antigen/bundles/zsh-users
-        git clone https://github.com/zsh-users/antigen.git $HOME/.antigen/bundles/zsh-users/antigen
-    fi
-    ln -f -s ${DOTFILES_DIR}/.zshrc ${HOME}
+  if [[ ! -d $HOME/.antigen ]]; then
+    mkdir -p $HOME/.antigen/bundles/zsh-users
+    git clone https://github.com/zsh-users/antigen.git $HOME/.antigen/bundles/zsh-users/antigen
+  fi
+  ln -f -s ${DOTFILES_DIR}/.zshrc ${HOME}
 }
 
 config_tmux() {
-    ln -f -s ${DOTFILES_DIR}/.tmux.conf ${HOME}
+  ln -f -s ${DOTFILES_DIR}/.tmux.conf ${HOME}
 }
 
 config_vim() {
-    if [[ ! -f  ~/.vim/autoload/plug.vim ]]; then
-        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    fi
-    ln -f -s ${DOTFILES_DIR}/.vimrc ${HOME}
-    vim +'PlugInstall --sync' +qa
+  if [[ ! -f  ~/.vim/autoload/plug.vim ]]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
+  ln -f -s ${DOTFILES_DIR}/.vimrc ${HOME}
+  vim +'PlugInstall --sync' +qa
 }
 
 config_emacs() {
-    if [[ ! -d $HOME/.emacs.d ]]; then
-        git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-        echo "Run the following command to setup doom:"
-        echo "~/.emacs.d/bin/doom -i quickstart"
-    fi
+  if [[ ! -d $HOME/.emacs.d ]]; then
+    git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+    echo "Run the following command to setup doom:"
+    echo "~/.emacs.d/bin/doom -i quickstart"
+  fi
 }
 
 main "$@"
--- bin/update.sh
+++ bin/update.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
-#
+
 main() {
-    set -o errexit
-    echo "Updating packages..."
-    read -p "Proceed? [y/N] : " yn
-    case $yn in
-        Y|y) update;;
-        *) ;;
-    esac
+  set -o errexit
+  echo "Updating packages..."
+  read -p "Proceed? [y/N] : " yn
+  case $yn in
+    Y|y) update;;
+    *) ;;
+  esac
 
-    echo "Updating zsh..."
-    zsh -i -c "antigen update"
-    zsh -i -c "antigen selfupdate"
+  echo "Updating zsh..."
+  zsh -i -c "antigen update"
+  zsh -i -c "antigen selfupdate"
 
-    echo "Updating vim..."
-    vim +'PlugUpdate --sync' +qa
-    vim +'PlugUpgrade --sync' +qa
+  echo "Updating vim..."
+  vim +'PlugUpdate --sync' +qa
+  vim +'PlugUpgrade --sync' +qa
 }
 
 update() {
-    UNAME=$(uname | tr "[:lower:]" "[:upper:]")
+  UNAME=$(uname | tr "[:lower:]" "[:upper:]")
 
-    case $UNAME in
-        DARWIN) update_mac ;;
+  case $UNAME in
+    DARWIN) update_mac ;;
 
-        LINUX)
-            DISTRO=$(cat /etc/os-release | grep ^ID= | cut -d '=' -f 2)
-            case $DISTRO in
-                ubuntu) update_linux ;;
-                *) give_up ;;
-            esac
-            ;;
-
+    LINUX)
+      DISTRO=$(cat /etc/os-release | grep ^ID= | cut -d '=' -f 2)
+      case $DISTRO in
+        ubuntu) update_linux ;;
         *) give_up ;;
-    esac
+      esac
+      ;;
+
+    *) give_up ;;
+  esac
 }
 
 give_up() {
@@ -42,12 +42,11 @@ give_up() {
 }
 
 update_mac() {
-    brew update && brew upgrade
+  brew update && brew upgrade
 }
 
 update_linux() {
-    sudo aptitude update && sudo aptitude upgrade
+  sudo aptitude update && sudo aptitude upgrade
 }
-
 
 main "$@"
