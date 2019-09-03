4b10b322c6102d1fd33dc0d83b1b6e0ae6edba7c
Chuan Liu <chuan.liu@oracle.com>
2019-09-03 11:17:04 -0700

    Add ccls for macOS

--- bin/install_lang.sh
+++ bin/install_lang.sh
@@ -37,26 +37,25 @@ install_rust() {
 
 install_ccls() {
   case $(_os) in
+    macos)
+      brew ls --version ccls || brew install ccls
+      ;;
     debian)
       sudo aptitude install -y zlib1g-dev libncurses-dev rapidjson-dev clang libclang-dev
-    ;;
-  esac
-
-  git clone --depth=1 --recursive https://github.com/MaskRay/ccls $XDG_DATA_HOME/ccls
-
-  pushd $XDG_DATA_HOME/ccls
-  git tag $(date +"%Y-%m-%d")
-  cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH=/usr/lib/llvm-8 \
-    -DLLVM_INCLUDE_DIR=/usr/lib/llvm-8/include
-  cmake --build Release
-
-  cat > $XDG_BIN_HOME/ccls<<EOF
+      git clone --depth=1 --recursive https://github.com/MaskRay/ccls $XDG_DATA_HOME/ccls
+      pushd $XDG_DATA_HOME/ccls
+      git tag $(date +"%Y-%m-%d")
+      cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_PREFIX_PATH=/usr/lib/llvm-8 \
+        -DLLVM_INCLUDE_DIR=/usr/lib/llvm-8/include
+      cmake --build Release
+      cat > $XDG_BIN_HOME/ccls<<EOF
 #!/usr/bin/env bash
 exec "$XDG_DATA_HOME/ccls/Release/ccls" "\$@"
 EOF
-
-  chmod +x $XDG_BIN_HOME/ccls
+      chmod +x $XDG_BIN_HOME/ccls
+      ;;
+  esac
 }
 
 main "$@"
